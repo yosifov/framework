09425ca0faf93bf242750f4b940aa1f1f14204cf
Yosifov <k.yosifov@gmail.com>
2020-01-31 23:25:54 +0200

    Initial Framework commit

---
+++ Framework/InitialTest.cs
@@ -0,0 +1,18 @@
+namespace Framework
+{
+    using NUnit.Framework;
+
+    public class InitialTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
